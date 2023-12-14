91c8fa96171beb66334fd6119f81c11156231366
Arda Çabuk <ardacabuk1@hotmail.com>
2023-12-14 17:36:17 +0300

    Lab05 added

---
+++ Lab05/ThreadedBST.cpp
@@ -0,0 +1,87 @@
+#include "ThreadedBST.h"
+
+///-----------------------------------------------
+/// Erases all nodes in the tree
+/// 
+void ThreadedBST::eraseTreeNodes(BSTNode* node) {
+	BSTNode *curr = this->min();
+
+	while (curr != NULL){
+		BSTNode *n = this->next(curr);
+		delete curr;
+		curr = n;
+	} // end-while
+	
+} //end-eraseTreeNodes
+
+///-----------------------------------------------
+/// Adds a given key to the BST
+/// 
+void ThreadedBST::add(int key) {
+	// Fill this in
+} // end-add
+
+///-----------------------------------------------
+/// Removes a given key from the BST (if it exists)
+/// 
+void ThreadedBST::remove(int key) {
+	// Fill this in
+} // end-remove
+
+///-----------------------------------------------
+/// Searches a given key in the ThreadedBST
+/// Return a pointer to the node that holds the key
+/// If the key is not found, return NULL
+/// 
+BSTNode *ThreadedBST::find(int key) {
+	// Fill this in
+	return NULL;
+} // end-find
+
+///-----------------------------------------------
+/// Returns the minimum key in the ThreadedBST
+/// Return a pointer to the node that holds the key
+/// If the key is not found, return NULL
+/// 
+BSTNode* ThreadedBST::min() {
+	BSTNode* node = root;
+	while (node->left) {
+		node = node->left;
+	}
+	return node;
+} // end-min
+
+///-----------------------------------------------
+/// Returns the maximum key in the ThreadedBST
+/// Return a pointer to the node that holds the key
+/// If the key is not found, return NULL
+/// 
+BSTNode* ThreadedBST::max() {
+	BSTNode* node = root;
+	while (root->right) {
+		node = node->right;
+	}
+	return node;
+} // end-max
+
+///-----------------------------------------------
+/// Given a valid pointer to a node in ThreadedBST,
+/// returns a pointer to the node that contains the inorder predecessor
+/// If the inorder predecessor does not exist, returns NULL
+/// 
+BSTNode* ThreadedBST::previous(BSTNode* node) {
+	if (node->leftLinkType == THREAD)
+		return node->left;
+	return NULL;
+} // end-previous
+
+///-----------------------------------------------
+/// Given a valid pointer to a node in the ThreadedBST,
+/// returns a pointer to the node that contains the inorder successor
+/// If the inorder successor does not exist, returns NULL
+/// 
+BSTNode* ThreadedBST::next(BSTNode* node) {
+	if (node->rightLinkType == THREAD)
+		return node->right;
+	return NULL;
+} // end-next
